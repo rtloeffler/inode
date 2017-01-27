40d2782975b402fae5921a054e2bb729adb3e4a3
Ryan <rtloeffler@users.noreply.github.com>
2017-01-27 14:17:05 -0800

    Update indoeuseage.sh

--- indoeuseage.sh
+++ indoeuseage.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 COUNT=`df -i|grep '^/dev'|awk '{print $6, 100 - $5}'|tr -d '// '`
 USED=20
-SERVER=agp-tn
+SERVER=`hostname`
 tstamp=`date +%m-%d-%Y:%H:%M:%S`
 
 if [[ $COUNT -lt $USED ]]; then
 echo $tstamp 
-echo 'Error: Inodes useage High, Only' \$COUNT 'Percent remaining on' \$SERVER \$tstamp | slacktee.sh -n -a "good" -o "warning" "^Warning:" -o "danger" "^Error:" -d "@here" "^Error:" -m link_names
+echo 'Error: Inodes useage High, Only' $COUNT 'Percent remaining on' $SERVER HS Server at $tstamp | slacktee.sh -n -a "good" -o "warning" "^Warning:" -o "danger" "^Error:" -d "@here" "^Error:" -m link_names
 else
 echo $tstamp ':inodes range range healthy currently' $COUNT 'percent remaining'
 fi
